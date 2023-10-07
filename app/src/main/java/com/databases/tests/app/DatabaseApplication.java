0633cfa27d19953d1b63f2510dd89bcc9f4ab1b2
aleokr <aleksandra.okrutny1@gmail.com>
2023-10-07 18:07:40 +0200

    database connection configs - redis & mongo

--- app/src/main/java/com/databases/tests/app/DatabaseApplication.java
+++ app/src/main/java/com/databases/tests/app/DatabaseApplication.java
@@ -2,10 +2,7 @@ package com.databases.tests.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
-
 @SpringBootApplication
-//@EnableCassandraRepositories(basePackages = "com.databases.tests.app.repository")
 public class DatabaseApplication {
 
 	public static void main(String[] args) {
